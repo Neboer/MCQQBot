64d10d85e80aee43e665fa5652288e2e96b67b82
Neboer <rubinposter@gmail.com>
2023-09-17 11:59:48 +0800

    1. 给bot添加了自定义事件功能。 2. 添加登录消息防抖的PlayerLoginManager 3. 修改玩家登录超时的判断逻辑，将30秒改为10秒

--- src/session_reporter.ts
+++ src/session_reporter.ts
@@ -1,7 +1,9 @@
 // 用来报告用户登录登出，上线下线的消息事务。
 // 用户上线报告、下线报告、连接报告
 import Bot from "./bot/bot";
+import logger from "./bot/logging";
+import MCServer from "./lighting_mc/minecraft_server";
 
-function bind_session_reporter(bot: Bot) {
-    // 玩家尝试连接
+function bind_session_reporter(bot: Bot, mc_server: MCServer) {
+
 }
