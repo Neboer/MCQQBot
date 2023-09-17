c2595550c5e4e77f71bcfc103d54d8ef747eb595
Neboer <rubinposter@gmail.com>
2023-09-18 07:42:18 +0800

    清晨提交，实现了PlayerSessionManager，完全绑定了机器人对玩家上下线的感知逻辑。不过还没有实现 正在为机器人寻找一个合适的字符串渲染模板引擎。

--- src/session_reporter.ts
+++ src/session_reporter.ts
@@ -2,19 +2,38 @@
 // 用户上线报告、下线报告、连接报告
 import Bot from "./bot/bot";
 import logger from "./bot/logging";
-import PlayerLoginManager from "./lighting_mc/minecraft_server";
+import PlayerSessionManager from "./lighting_mc/minecraft_server";
+import {PlayerOnlineState} from "./lighting_mc/serverPlayer";
 
 function bind_session_reporter(bot: Bot) {
-    const player_login_manager = new PlayerLoginManager()
+    const player_session_manager = new PlayerSessionManager()
 
-    // bind basic ev.
     bot.on_mc_log(/^UUID of player (?<name>.*?) is (?<uuid>.{36})$/, (bot_instance, m_mc_msg) => {
         logger.info(`uuid ${m_mc_msg.matched_groups.name}`)
-        player_login_manager.player_connect(m_mc_msg.matched_groups.name)
+        player_session_manager.player_connect(m_mc_msg.matched_groups.name)
     })
 
     bot.on_mc_log(/^(?<name>.*?)\[\/(?<ip_addr>.*?):\d+] logged in with entity id .*/, async (bot_instance, m_mc_msg) => {
         logger.info(`login ${m_mc_msg.matched_groups.name}`)
-        player_login_manager.player_connect(m_mc_msg.matched_groups.name)
+        player_session_manager.player_connect(m_mc_msg.matched_groups.name)
+    })
+
+    bot.on_mc_log(/^(?<name>.*?) joined the game/, async (bot_instance, m_mc_msg) => {
+        logger.info(`join ${m_mc_msg.matched_groups.name}`)
+        player_session_manager.player_login(m_mc_msg.matched_groups.name)
+    })
+
+    bot.on_mc_log(/^(?<name>.*?) lost connection/, async (bot_instance, m_mc_msg) => {
+        logger.info(`lost connection ${m_mc_msg.matched_groups.name}`)
+        player_session_manager.player_exit(m_mc_msg.matched_groups.name)
+    })
+
+    bot.on_mc_log(/^(?<name>.*?) left the game$/, async (bot_instance, m_mc_msg) => {
+        logger.info(`lost connection ${m_mc_msg.matched_groups.name}`)
+        player_session_manager.player_exit(m_mc_msg.matched_groups.name)
+    })
+
+    bot.on_event('player_login_failed', (player_name) => {
+
     })
 }
