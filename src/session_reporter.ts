8038b97724a10f3397ea97025c2c2c8ebb65dc28
Neboer <rubinposter@gmail.com>
2023-09-20 22:52:28 +0800

    深夜提交（我好肝 实现了字符串随机选择发送消息的功能。 可以实时组装待发送的内容。 升级到0.2.1版本（测试 将之前main中的功能拆到不同的模块中 没有测试之前连接的问题是否被修复了，不过我想应该已经修复了。

--- src/session_reporter.ts
+++ src/session_reporter.ts
@@ -3,9 +3,9 @@
 import Bot from "./bot/bot";
 import logger from "./bot/logging";
 import PlayerSessionManager from "./lighting_mc/minecraft_server";
-import {PlayerOnlineState} from "./lighting_mc/serverPlayer";
+import TextBuilder from "./TextBuilder";
 
-function bind_session_reporter(bot: Bot) {
+export default function bind_session_reporter(bot: Bot, text_builder: TextBuilder) {
     const player_session_manager = new PlayerSessionManager()
 
     bot.on_mc_log(/^UUID of player (?<name>.*?) is (?<uuid>.{36})$/, (bot_instance, m_mc_msg) => {
@@ -34,6 +34,30 @@ function bind_session_reporter(bot: Bot) {
     })
 
     bot.on_event('player_login_failed', (player_name) => {
+        logger.info(`player_login_failed ${player_name}`)
+        const broadcast_msg = text_builder.build_random_translate_str('玩家.登录.玩家登录失败广播', {name: player_name})
+        bot.broadcast_mc_message(broadcast_msg)
+        bot.send_default_qqgroup_message(broadcast_msg)
+    })
 
+    bot.on_event('player_connected', player_name => {
+        logger.info(`player_connected ${player_name}`)
+        const broadcast_msg = text_builder.build_random_translate_str('玩家.连接.老玩家连接广播', {name: player_name})
+        bot.broadcast_mc_message(broadcast_msg)
+        bot.send_default_qqgroup_message(broadcast_msg)
+    })
+
+    bot.on_event('player_login_success', player_name => {
+        logger.info(`player_login_success ${player_name}`)
+        const broadcast_msg = text_builder.build_random_translate_str('玩家.登录.玩家登录广播', {name: player_name})
+        bot.broadcast_mc_message(broadcast_msg)
+        bot.send_default_qqgroup_message(broadcast_msg)
+    })
+
+    bot.on_event('player_exit', player_name => {
+        logger.info(`player_exit ${player_name}`)
+        const broadcast_msg = text_builder.build_random_translate_str('玩家.登出.玩家登出广播', {name: player_name})
+        bot.broadcast_mc_message(broadcast_msg)
+        bot.send_default_qqgroup_message(broadcast_msg)
     })
 }
