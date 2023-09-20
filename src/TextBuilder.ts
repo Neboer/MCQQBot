81f480a4ba2e36f4a56c602f341a59c8d58f3c85
Neboer <rubinposter@gmail.com>
2023-09-20 17:51:17 +0800

    上课更新,日志开始检查环境变量了,基本连接出错被修复了,开始编写机器人回复文本构造器

--- src/TextBuilder.ts
+++ src/TextBuilder.ts
@@ -1,10 +1,19 @@
-import {Eta} from "eta"
+import { Eta } from "eta"
+import { readFileSync } from "fs"
+import { parse } from "yaml"
+
+export type TranslateDoc = {[key: string]: TranslateDoc|string[]}
 
 // 组装字符串！
 class TextBuilder {
-    engine: Eta = new Eta()
+  engine: Eta = new Eta()
+  msg_templates: TranslateDoc
+  constructor() {
+    const translate_file_content = readFileSync('../../config/string.yaml', {encoding: 'utf-8'})
+    this.msg_templates = parse(translate_file_content)
+  }
 
-    constructor() {
-
-    }
+  get_translate_str() {
+    
+  }
 }
--- src/bot/logging.ts
+++ src/bot/logging.ts
@@ -1,13 +1,16 @@
 import pino from "pino";
 
-
-const logger = pino({
+const is_dev = process.env.NODE_ENV == "development"
+const pino_obj = is_dev ?
+  {
     transport: {
-        target: 'pino-pretty',
-        options: {
-            colorize: true
-        }
+      target: 'pino-pretty',
+      options: {
+        colorize: true
+      }
     },
     level: 'debug'
-})
+  } : {};
+
+const logger = pino(pino_obj);
 export default logger;
