87e65e73362e8ea985c028378c2f8ebb861eaf2c
Neboer <rubinposter@gmail.com>
2023-09-29 21:29:33 +0800

    紧急修复一个可能导致玩家日志被反复展示的bug

--- src/bot_function/session_reporter.ts
+++ src/bot_function/session_reporter.ts
@@ -42,25 +42,37 @@ export default function bind_session_reporter(bot: Bot, text_builder: TextBuilde
     //     bot.send_default_qqgroup_message(broadcast_msg)
     // })
 
-    bot.on_event('player_connected', player_name => {
-        logger.info(`player_connected ${player_name}`)
-        // TODO: 新玩家检测，新玩家用户名合规判断
-        const broadcast_msg = text_builder.build_random_translate_str('玩家.连接.老玩家连接广播', {name: player_name})
-        bot.broadcast_mc_message(broadcast_msg)
-        bot.send_default_qqgroup_message(broadcast_msg)
+    bot.on_event('player_connected', (player_name: string) => {
+        if (!player_name.startsWith(`com.mojang.authlib.GameProfile@`)) {
+            logger.info(`player_connected ${player_name}`)
+            // TODO: 新玩家检测，新玩家用户名合规判断
+            const broadcast_msg = text_builder.build_random_translate_str('玩家.连接.老玩家连接广播', {name: player_name})
+            bot.broadcast_mc_message(broadcast_msg)
+            bot.send_default_qqgroup_message(broadcast_msg)
+        } else {
+            logger.info(`player_connected authlib: ${player_name}`)
+        }
     })
 
-    bot.on_event('player_login_success', player_name => {
-        logger.info(`player_login_success ${player_name}`)
-        const broadcast_msg = text_builder.build_random_translate_str('玩家.登录.玩家登录广播', {name: player_name})
-        bot.broadcast_mc_message(broadcast_msg)
-        bot.send_default_qqgroup_message(broadcast_msg)
+    bot.on_event('player_login_success', (player_name: string) => {
+        if (!player_name.startsWith(`com.mojang.authlib.GameProfile@`)) {
+            logger.info(`player_login_success ${player_name}`)
+            const broadcast_msg = text_builder.build_random_translate_str('玩家.登录.玩家登录广播', {name: player_name})
+            bot.broadcast_mc_message(broadcast_msg)
+            bot.send_default_qqgroup_message(broadcast_msg)
+        } else {
+            logger.info(`player_login authlib: ${player_name}`)
+        }
     })
 
-    bot.on_event('player_exit', player_name => {
-        logger.info(`player_exit ${player_name}`)
-        const broadcast_msg = text_builder.build_random_translate_str('玩家.登出.玩家登出广播', {name: player_name})
-        bot.broadcast_mc_message(broadcast_msg)
-        bot.send_default_qqgroup_message(broadcast_msg)
+    bot.on_event('player_exit', (player_name: string) => {
+        if (!player_name.startsWith(`com.mojang.authlib.GameProfile@`)) {
+            logger.info(`player_exit ${player_name}`)
+            const broadcast_msg = text_builder.build_random_translate_str('玩家.登出.玩家登出广播', {name: player_name})
+            bot.broadcast_mc_message(broadcast_msg)
+            bot.send_default_qqgroup_message(broadcast_msg)
+        } else {
+            logger.info(`player_exit authlib: ${player_name}`)
+        }
     })
 }
